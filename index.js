c684c4f9a1ae2cb09522ab9302337f3dee5f4855
SanjeevanRam <144502397+SanjeevanRam@users.noreply.github.com>
2024-09-01 10:14:09 +0530

    Add files via upload
    Add HTML,CSS,JS

---
+++ index.js
@@ -0,0 +1,64 @@
+/***************** toggel Icon navbar ***********************/ 
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = ()=>{
+    // menuIcon.classList.toggle('fa-xmark');
+    // navbar.classList.toggle('active');
+    if(menuIcon.classList.contains("fa-bars")) {
+        menuIcon.classList.remove("fa-bars");
+        menuIcon.classList.add("fa-times");
+        navbar.style.display="block ";
+    } else {
+        menuIcon.classList.add("fa-bars");
+        menuIcon.classList.remove("fa-times");
+        navbar.style.display='none';
+    }
+    // navbar.style.display =(navbar.style.display === 'none') ? 'block' :'none';
+    // menuIcon.classList.toggle('fa-xmark');
+}
+
+
+/***************** Scroll section active link ***********************/ 
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    /***************** sticky navbar ***********************/
+    
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky' , window.scrollY > 100);
+
+
+    /***************** remove toggel icon and navbar ***********************/ 
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active') ;
+}
+
+
+/Typed js*/
+
+const typed =
+new Typed(".multiple-text",{
+    strings: ["Data Analyst", "Power BI Tool." , "Data models."],
+    typeSpeed : 70,
+    backSpeed:70,
+    backDelay:1000,
+    loop: true,
+});
